cc87f03d46b21b68225a624b81e2a0f2f37eb0c2
suyang-hk <ysu923443@gmail.com>
2025-04-01 17:47:33 +0800

    Update vector : allocater,iterator.cpp

--- vector : allocater,iterator.cpp
+++ vector : allocater,iterator.cpp
@@ -85,7 +85,7 @@ public:
         if(empty()){
             return ;
         }
-
+        verify(_last -1 ,_last);
         _last--;
         _allocater.destory(_last);
 
@@ -115,17 +115,32 @@ public:
 
     class iterator {
     public:
-        iterator(T* p = nullptr) : _ptr(p){};
+        friend class vector<T,Alloc>;
+        iterator(vector<T,Alloc>* _pvec = nullptr ,T* p = nullptr)
+        : _ptr(p), _pvec(_pvec){
+            Iterator_Base *itb = new Iterator_Base(this , _pvec->_head.next);
+            _pvec->_head.next = itb;
+        };
+
         bool operator!=(const iterator& it) const {
+            if(_pvec == nullptr || _pvec != it._pvec){
+                throw "iterator incompatable!";
+            }
             return _ptr != it._ptr;
         }
 
         iterator &operator++(){
+            if(_pvec == nullptr){
+                throw "iterator invalid!";
+            }
             ++_ptr;
             return *this;
         }
 
         T& operator*(){
+            if(_pvec == nullptr){
+                throw "iterator invalid!";
+            }
             return *_ptr;
         }
 
@@ -134,16 +149,73 @@ public:
         }
     private:
         T* _ptr;
-
+        vector<T,Alloc> *_pvec;
     };
 
-    iterator begin(){ return iterator(_first); }
-    iterator end(){ return  iterator(_last); }
+    iterator begin(){ return iterator(this , _first); }
+    iterator end(){ return  iterator(this , _last); }
+
+    iterator insert(iterator it , const T &val){
+        /*
+         * 不考虑扩容
+         * 不考虑空指针 it._ptr 的合法性
+         */
+        verify(it._ptr - 1, _last);
+        T* p = _last;
+        while(p > it._ptr ){
+            _allocater.construct(p,*(p -1 ));
+            _allocater.destory(p - 1);
+            --p;
+        }
+        _allocater.construct(p,val);
+        _last++;
+        return iterator(this , p);
+    }
+
+    iterator erase( iterator it ){
+        verify(it._ptr - 1 , _last);
+        T* p = it._ptr;
+        while (p != _last - 1){
+            _allocater.destory(p);
+            _allocater.construct(p ,*(p + 1));
+            ++p;
+        }
+        _allocater.destory(p);
+        _last--;
+        return iterator(this , it._ptr);
+    }
 private:
     T* _first;
     T* _last;
     T* _end;
     Alloc _allocater;
+
+        struct Iterator_Base
+        {
+            Iterator_Base(iterator* c = nullptr, Iterator_Base *n = nullptr)
+            : _cur(c) , next(n){}
+            iterator *_cur;
+            Iterator_Base* next;
+        };
+        Iterator_Base _head;
+
+        //检查迭代器失效
+        void verify(T *first , T *last) {
+            Iterator_Base *pre = &this->_head;
+            Iterator_Base *it = this->_head.next;
+            while ( it != nullptr){
+                if(it->_cur->_ptr > first &&  it->_cur->_ptr  <= last){
+                    it->_cur->_pvec = nullptr ;
+                    pre->next = it->next;
+                    delete it;
+                    it = pre->next;
+                } else {
+                    pre = it;
+                    it = it->next;
+                }
+            }
+        }
+
 
     void expand() {
         int size = _last - _first;
@@ -164,7 +236,7 @@ private:
 
 int main(){
     srand(time(0));
-   vector<int> a;
+   vector<int> a(200);
 
     for (int i = 0; i < 20; ++i) {
         a.push_back(rand()%100 + 1);
@@ -179,7 +251,12 @@ int main(){
     cout << "----------------------" << endl;
 
     for(; it != a.end(); ++it){
-        cout << *it << '.';
+        it = a.insert(it , *it - 1);
+        ++it;
+    }
+
+    for(int v : a){
+        cout << v << ' ';
     }
 
     cout << endl;
